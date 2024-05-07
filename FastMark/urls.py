b35745b8493f9987b5bcefd676340068716838cf
AmrAlt <112940374+amralt@users.noreply.github.com>
2024-05-07 10:51:23 +0600

    practice 2 finished

--- FastMark/urls.py
+++ FastMark/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:article_id>/', views.get_article, name = 'article'),
+    path('about', views.get_about, name='about'),
 ]
